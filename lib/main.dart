206c6b1d3cb4f7eb619dccb8e33e3c87cbaefe11
phanbaohuy96 <2001140087@ittc.edu.vn>
2019-06-14 22:22:03 +0700

    -change first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:profile_manager/views/pages/login_page.dart';
 import 'package:profile_manager/views/pages/menu_dashboard_page.dart';
-import 'package:profile_manager/views/pages/profile_info_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MenuDashboardPage(),
+      //home: MenuDashboardPage(),
+      home: LoginPage(),
     );
   }
 }
