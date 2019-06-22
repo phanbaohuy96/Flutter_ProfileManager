909d1b81833cdef3e4595ec5fe5a35ae79a7a55a
phanbaohuy96 <2001140087@ittc.edu.vn>
2019-06-22 16:32:24 +0700

    - add setting_page.dart

---
+++ lib/views/pages/management_page/setting_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+
+  final bool isCollapsed;
+  final Color mainColor;
+
+  const SettingPage({Key key, this.isCollapsed, this.mainColor}) : super(key: key);
+
+
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
